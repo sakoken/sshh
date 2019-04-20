5eaf190915b3e79d030f25fa50f79a32ace55ee4
Kenji Sakoda <jones.with.maul@gmail.com>
2019-04-20 18:30:54 +0900

    ver up

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 	app := cli.App{}
 	app.Name = "sshh"
 	app.Description = "sshh is a management application of hosts for ssh"
-	app.Version = "0.0.4"
+	app.Version = "0.0.10"
 	app.Before = before()
 	app.Action = func(c *cli.Context) error {
 		if c.Args().First() != "" {
